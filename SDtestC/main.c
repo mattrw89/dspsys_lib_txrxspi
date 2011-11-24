890b52a674b258255c977778962cfec8931b5605
Matt Webb <mattrw89@gmail.com>
2011-11-23 21:17:38 -0500

    Initial Commit

---
+++ SDtestC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SDtestC
+//
+//  Created by Matt Webb on 11/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
